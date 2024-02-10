d4b3bd865f51d30e43fa896766bc8fa22c0252c2
bealecs <cliftonscottbeale@gmail.com>
2024-02-09 18:58:07 -0600

    some more changes to the two main components

--- clif-codes/src/app/components/Header.tsx
+++ clif-codes/src/app/components/Header.tsx
@@ -1,6 +1,6 @@
-"use client"
-import { Fragment, useState } from 'react'
-import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
+"use client";
+import { Fragment, useState } from "react";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
 import {
   ArrowPathIcon,
   Bars3Icon,
@@ -9,40 +9,75 @@ import {
   FingerPrintIcon,
   SquaresPlusIcon,
   XMarkIcon,
-} from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
-import { Great_Vibes } from 'next/font/google';
+} from "@heroicons/react/24/outline";
+import {
+  ChevronDownIcon,
+  PhoneIcon,
+  PlayCircleIcon,
+} from "@heroicons/react/20/solid";
+import { Great_Vibes } from "next/font/google";
 
 const greatVibes = Great_Vibes({
-    weight: '400',
-    subsets: ['latin'],
-    variable: '--font-great-vibes'
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-great-vibes",
 });
 
 const products = [
-  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-  { name: 'UI Design', description: 'Bring your site to life with cutting-edge UI development', href: '#', icon: CursorArrowRaysIcon },
-  { name: 'User Authentication Services', description: 'Allow users sign in or create accounts', href: '#', icon: SquaresPlusIcon },
-  { name: 'Database Setup & Management', description: 'Manage your data & information in a safe and secure environment', href: '#', icon: FingerPrintIcon },
-  { name: 'Email & Subscription Services', description: 'Get set up with newsletter & subscription services for your users', href: '#', icon: ArrowPathIcon },
-]
+  {
+    name: "Analytics",
+    description: "Get a better understanding of your traffic",
+    href: "#",
+    icon: ChartPieIcon,
+  },
+  {
+    name: "UI Design",
+    description: "Bring your site to life with cutting-edge UI development",
+    href: "#",
+    icon: CursorArrowRaysIcon,
+  },
+  {
+    name: "User Authentication Services",
+    description: "Allow users sign in or create accounts",
+    href: "#",
+    icon: SquaresPlusIcon,
+  },
+  {
+    name: "Database Setup & Management",
+    description:
+      "Manage your data & information in a safe and secure environment",
+    href: "#",
+    icon: FingerPrintIcon,
+  },
+  {
+    name: "Email & Subscription Services",
+    description:
+      "Get set up with newsletter & subscription services for your users",
+    href: "#",
+    icon: ArrowPathIcon,
+  },
+];
 const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact Clif', href: '#', icon: PhoneIcon },
-]
+  { name: "Watch demo", href: "#", icon: PlayCircleIcon },
+  { name: "Contact Clif", href: "#", icon: PhoneIcon },
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Header() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <header className="bg-white">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
         <div className="flex lg:flex-1">
-            <h1 className={`${greatVibes} text-black-400 pr-2 text-5xl`}>Clif</h1><h1 className="text-sky-400 text-5xl">Codes</h1>     
+          <h1 className={`${greatVibes} text-black-400 pr-2 text-4xl`}>Clif</h1>
+          <h1 className="text-sky-400 text-4xl">Codes</h1>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -58,7 +93,10 @@ export default function Header() {
           <Popover className="relative">
             <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
               Services
-              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+              <ChevronDownIcon
+                className="h-5 w-5 flex-none text-gray-400"
+                aria-hidden="true"
+              />
             </Popover.Button>
 
             <Transition
@@ -78,10 +116,16 @@ export default function Header() {
                       className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
                     >
                       <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
+                        <item.icon
+                          className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
+                          aria-hidden="true"
+                        />
                       </div>
                       <div className="flex-auto">
-                        <a href={item.href} className="block font-semibold text-gray-900">
+                        <a
+                          href={item.href}
+                          className="block font-semibold text-gray-900"
+                        >
                           {item.name}
                           <span className="absolute inset-0" />
                         </a>
@@ -97,7 +141,10 @@ export default function Header() {
                       href={item.href}
                       className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
                     >
-                      <item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+                      <item.icon
+                        className="h-5 w-5 flex-none text-gray-400"
+                        aria-hidden="true"
+                      />
                       {item.name}
                     </a>
                   ))}
@@ -109,7 +156,11 @@ export default function Header() {
           <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
             Schedule Consultation
           </a>
-          <a href="https://clif.codes" target='_blank' className="text-sm font-semibold leading-6 text-gray-900">
+          <a
+            href="https://clif.codes"
+            target="_blank"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
             Portfolio
           </a>
           <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
@@ -117,18 +168,20 @@ export default function Header() {
           </a>
         </Popover.Group>
       </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Clif Codes</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
+          <div className="flex justify-between">
+            <div className="flex">
+              <h1 className={`${greatVibes} text-black-400 pr-2`}>Clif</h1>
+              <h1 className="text-sky-400">Codes</h1>
+            </div>
+
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -145,9 +198,12 @@ export default function Header() {
                   {({ open }) => (
                     <>
                       <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                        Product
+                        Services
                         <ChevronDownIcon
-                          className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
+                          className={classNames(
+                            open ? "rotate-180" : "",
+                            "h-5 w-5 flex-none"
+                          )}
                           aria-hidden="true"
                         />
                       </Disclosure.Button>
@@ -190,5 +246,5 @@ export default function Header() {
         </Dialog.Panel>
       </Dialog>
     </header>
-  )
+  );
 }
--- clif-codes/src/app/components/Hero.tsx
+++ clif-codes/src/app/components/Hero.tsx
@@ -1,23 +1,29 @@
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import {
+  CloudArrowUpIcon,
+  LockClosedIcon,
+  ServerIcon,
+} from "@heroicons/react/20/solid";
 
 const features = [
   {
-    name: 'Push to deploy.',
+    name: "Full Stack Web Application Solutions",
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      "Clif Codes offers a range of solutions, from static, single-page web applications to dynamic full-stack applications. Schedule a consult to find your solution.",
     icon: CloudArrowUpIcon,
   },
   {
-    name: 'SSL certificates.',
-    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    name: "Optimized Performance, Accessibility, and SEO",
+    description:
+      "No matter your needs, Clif Codes develops with Accessibility, Performance, and SEO at the forefront of every solution.",
     icon: LockClosedIcon,
   },
   {
-    name: 'Database backups.',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    name: "Cost Effective Solutions",
+    description:
+      "Clif Codes works quickly and efficiently, while using the most up-to-date technologies. In doing so, we ensure that our clients get lower prices for better solutions",
     icon: ServerIcon,
   },
-]
+];
 
 export default function Example() {
   return (
@@ -26,34 +32,43 @@ export default function Example() {
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</p>
+              <h2 className="text-base font-semibold leading-7 text-indigo-600">
+                I do it better
+              </h2>
+              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                Guaranteed Satisfaction
+              </p>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-                iste dolor cupiditate blanditiis ratione.
+                I care deeply about my work, and won't stop until I get the
+                perfect solution for your needs.
               </p>
               <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                 {features.map((feature) => (
                   <div key={feature.name} className="relative pl-9">
                     <dt className="inline font-semibold text-gray-900">
-                      <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-600" aria-hidden="true" />
+                      <feature.icon
+                        className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                        aria-hidden="true"
+                      />
                       {feature.name}
-                    </dt>{' '}
+                    </dt>{" "}
                     <dd className="inline">{feature.description}</dd>
                   </div>
                 ))}
               </dl>
             </div>
           </div>
-          <img
-            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-            alt="Product screenshot"
-            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-            width={2432}
-            height={1442}
-          />
+          <div className="flex justify-center p-8">
+            <img
+              src="https://img.freepik.com/free-photo/programming-background-with-person-working-with-codes-computer_23-2150010125.jpg?w=1380&t=st=1707525998~exp=1707526598~hmac=62c7cf4dcd525d8dae5205765ee0094fd326da4e1dbd811765c03b6c18985e77"
+              alt="Screenshot of someone coding"
+              className="rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+              width={2432}
+              height={1442}
+            />
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
