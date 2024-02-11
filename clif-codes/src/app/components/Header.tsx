185feaee60c46deab5c93c5063a273f2b2a17fa3
bealecs <cliftonscottbeale@gmail.com>
2024-02-11 13:20:32 -0600

    Random push to create pr

--- clif-codes/src/app/components/Header.tsx
+++ clif-codes/src/app/components/Header.tsx
@@ -72,7 +72,7 @@ export default function Header() {
   return (
     <header className="bg-white">
       <nav
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-7xl items-center content-center justify-between p-6 lg:px-8"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
