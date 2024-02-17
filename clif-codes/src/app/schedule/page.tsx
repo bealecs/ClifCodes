d244bb50816e34ab1468ce848c11cc0d8afacda9
bealecs <cliftonscottbeale@gmail.com>
2024-02-16 22:46:45 -0600

    added footer to schedule and contact pages

--- clif-codes/src/app/schedule/page.tsx
+++ clif-codes/src/app/schedule/page.tsx
@@ -2,12 +2,14 @@
 import React from "react";
 import { InlineWidget } from "react-calendly";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Schedule = () => {
   return (
     <div>
       <Header />
       <InlineWidget url="https://calendly.com/clifcodes/consult" />
+      <Footer />
     </div>
   );
 };
