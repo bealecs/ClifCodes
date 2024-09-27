08e8b3d4162bff097e313d18686dc53cd2243572
bealecs <cliftonscottbeale@gmail.com>
2024-09-27 15:09:51 -0500

    fixed up contact page links

--- clif-codes/src/app/contact/page.tsx
+++ clif-codes/src/app/contact/page.tsx
@@ -8,7 +8,7 @@ export default function Contact() {
     <>
       <Header />
       <div className="flex justify-around max-w-5xl m-auto pt-[4rem] items-center content-center sm:flex-col sm:pt-[2rem]">
-        <div className="flex flex-col justify-between w-[50%] sm:w-[75%]">
+        <div className="flex flex-col justify-between w-[50%] sm:w-[100%] sm:px-8">
           <h2 className="font-bold text-3xl">Point of Contact</h2>
           <a href="/schedule" className="mt-2 border-2 w-fit rounded px-1 transition duration-300 linear hover:bg-zinc-200 border-sky-500 hover:border-blue-300 hover:text-blue-600">Schedule Consultation</a>
           <p className="pt-[2rem]">
@@ -25,7 +25,7 @@ export default function Contact() {
           <h4 className="text-sky-500 pt-[1rem]">Freelance Web Developer</h4>
         </div>
       </div>
-      <aside className="text-gray-500 text-center py-4 underline italic">Note: The buttons below are external links</aside>
+      <aside className="text-gray-500 text-center py-4 underline italic">Note: Some buttons below are external links</aside>
       <div className="flex pt-8 pb-[6rem] w-[50%] mx-auto justify-evenly sm:pt-[2rem]">
         <SocialIcon
           bgColor="rgb(56 189 248)"
@@ -37,15 +37,14 @@ export default function Contact() {
           bgColor="rgb(56 189 248)"
           href="https://clifcodes.dev/schedule"
           className="transition duration-300 ease-in-out border-[0.12rem] rounded-[100%] border-black hover:scale-125"
-          target="_blank"
           network="email"
         />
         <SocialIcon
           bgColor="rgb(56 189 248)"
-          href="https://discord.com/users/367069635083894784"
+          href="https://github.com/bealecs?tab=repositories"
           className="transition duration-300 ease-in-out border-[0.12rem] rounded-[100%] border-black hover:scale-125 "
           target="_blank"
-          network="discord"
+          network="github"
         />
       </div>
       <Footer />
